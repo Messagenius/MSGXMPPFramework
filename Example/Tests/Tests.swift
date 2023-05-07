25c0dafd5bcd434400c4ed0975ca04f97791a709
Francesco Cosentino <francesco@messagenius.com>
2023-05-08 00:49:18 +0200

    Small change

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,9 +5,7 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        let stream = XMPPStream()
-        let message = XMPPMessage()
-        message.addComposingChatState()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
